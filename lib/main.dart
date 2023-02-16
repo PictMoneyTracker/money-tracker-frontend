4838f31f125c44de825a7a89c872eba0f79f967a
mundadapurva <mundadapurva02@gmail.com>
2023-02-16 12:11:06 +0530

    PR tested on android

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   // test command
-  FirebaseCRUDApiService.createDoc();
+  // FirebaseCRUDApiService.createDoc();
   runApp(const MyApp());
 }
 

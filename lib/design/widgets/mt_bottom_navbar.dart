d6206e1be1467e58fd55df3f938dbd155f03314d
Atharv Bhadange <65965314+atharv-bhadange@users.noreply.github.com>
2023-03-01 11:14:57 +0530

    bottom nav bar with bloc (#7)

--- lib/design/widgets/mt_bottom_navbar.dart
+++ lib/design/widgets/mt_bottom_navbar.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../features/dashboard/bloc/dashboard_bloc.dart';
 
 class MtBottomNavbar extends StatelessWidget {
   const MtBottomNavbar({super.key});
@@ -9,8 +13,11 @@ class MtBottomNavbar extends StatelessWidget {
       iconSize: 25,
       selectedFontSize: 16,
       unselectedFontSize: 13,
-      // currentIndex: currentIndex,
-      // onTap: (index) => setState(() => currentIndex = index),
+      currentIndex: context.select((DashboardBloc bloc) => bloc.currentIndex),
+      onTap: (index) {
+        BlocProvider.of<DashboardBloc>(context)
+            .add(DashboardIndexChangedEvent(index));
+      },
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.bar_chart),

a88768866555261505be1d70c4511be04a1bdc30
mundadapurva <mundadapurva02@gmail.com>
2023-03-07 18:59:21 +0530

    styled loader and balance card

--- lib/features/dashboard/ui/widgets/balance_card.dart
+++ lib/features/dashboard/ui/widgets/balance_card.dart
@@ -13,60 +13,105 @@ class BalanceCard extends StatelessWidget {
         color: Colors.blue,
       ),
       margin: const EdgeInsets.all(8),
-      height: 180,
+      height: 170,
       alignment: Alignment.center,
       width: double.infinity,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            "Lorem Ipsum",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
           Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(Icons.arrow_upward),
-                const SizedBox(
-                  width: 5,
-                ),
-                Container(
-                  margin: const EdgeInsets.all(8),
-                  child: const Text(
-                    'Lorem Ipsum',
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.blue,
+              ),
+              margin: const EdgeInsets.all(8),
+              height: 150,
+              alignment: Alignment.center,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Total Balance",
                     style: TextStyle(
                       fontSize: 15,
                       color: Colors.white,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 40,
-                ),
-                const Icon(Icons.arrow_downward),
-                const SizedBox(
-                  width: 5,
-                ),
-                Container(
-                  margin: const EdgeInsets.all(8),
-                  child: const Text(
-                    'Lorem Ipsum',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.currency_rupee,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      Text(
+                        "100",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
-          )
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.arrow_upward_rounded,
+                        color: Color.fromARGB(255, 0, 158, 5),
+                        size: 30,
+                      ),
+                      Text(
+                        "Income",
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 80,
+                      ),
+                      Icon(Icons.arrow_downward_rounded,
+                          color: Colors.red, size: 30),
+                      Text(
+                        "Expenses",
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.currency_rupee, color: Colors.white, size: 20),
+                      Text(
+                        "10000",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 80,
+                      ),
+                      Icon(Icons.currency_rupee, color: Colors.white, size: 20),
+                      Text(
+                        "100",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )),
         ],
       ),
     );

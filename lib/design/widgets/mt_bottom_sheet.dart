9ce07a212aeddafd273c74a06b610d9f78238b5c
atharv-bhadange <bhadange.atharv@gmail.com>
2023-03-07 23:05:56 +0530

    fixed modal bottom sheet text field issue & completed end to end transaction feature

--- lib/design/widgets/mt_bottom_sheet.dart
+++ lib/design/widgets/mt_bottom_sheet.dart
@@ -1,136 +1,182 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:money_tracker/core/api_service/firebase_crud_service/utils/categories.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uuid/uuid.dart';
 
-class MtBottomSheet extends StatelessWidget {
-  MtBottomSheet({super.key});
+import '../../core/api_service/firebase_crud_service/transaction_service/models/transaction_model.dart';
+import '../../core/api_service/firebase_crud_service/transaction_service/transaction_service.dart';
+import '../../core/api_service/firebase_crud_service/utils/categories.dart';
+import '../../features/dashboard/bloc/dashboard_bloc.dart';
+
+class MtBottomSheet extends StatefulWidget {
+  const MtBottomSheet({
+    Key? key,
+    required this.spentFrom,
+  }) : super(key: key);
+
+  final SpendFrom spentFrom;
+  @override
+  State<MtBottomSheet> createState() => _MtBottomSheetState();
+}
+
+class _MtBottomSheetState extends State<MtBottomSheet> {
   static final formKey = GlobalKey<FormState>();
   final TextEditingController titleController = TextEditingController();
+
   final TextEditingController descriptionController = TextEditingController();
+
   final TextEditingController amountController = TextEditingController();
+
+  final items = SpendCategory.values.toList();
+  var dropdownValue = SpendCategory.food;
+
   @override
   Widget build(BuildContext context) {
-    String dropdownvalue = 'Item 1';
-    final items = SpendCategory.values.toList();
-    return Container(
-      height: 640,
-      padding: EdgeInsets.only(
-          bottom: MediaQuery.of(context).viewInsets.bottom,
-          top: 20,
-          left: 20,
-          right: 20),
-      child: Form(
-        key: formKey,
-        child: ListView(
-          // mainAxisSize: MainAxisSize.min,
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            const Text(
-              "Title",
-              style: TextStyle(fontSize: 15, color: Colors.blue),
-            ),
-            TextFormField(
-              maxLength: 50,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              controller: titleController,
-              decoration: const InputDecoration(
-                hintText: 'Enter your Title',
-                border: OutlineInputBorder(),
-                counter: Offstage(),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-              ),
-            ),
-            const Text(
-              "Description",
-              style: TextStyle(fontSize: 15, color: Colors.blue),
-            ),
-            TextFormField(
-              maxLength: 150,
-              minLines: 1,
-              maxLines: 10,
-              controller: descriptionController,
-              decoration: const InputDecoration(
-                hintText: 'Enter your Description',
-                border: OutlineInputBorder(),
-                counter: Offstage(),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-              ),
-            ),
-            const Text(
-              "Amount",
-              style: TextStyle(fontSize: 15, color: Colors.blue),
-            ),
-            TextFormField(
-              validator: (value) {
-                String pattern = r'^\d+(?:\.\d+)?$';
-                RegExp regex = RegExp(pattern);
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                } else if (!regex.hasMatch(value)) {
-                  return 'Enter Valid Number';
-                } else {
-                  return null;
-                }
-              },
-              maxLength: 10,
-              controller: amountController,
-              decoration: const InputDecoration(
-                hintText: 'Enter your amount',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.currency_rupee),
-                counter: Offstage(),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-              ),
-            ),
-            const Text(
-              "Category",
-              style: TextStyle(fontSize: 15, color: Colors.blue),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return BlocBuilder<DashboardBloc, DashboardState>(
+      builder: (context, state) {
+        return Container(
+          height: 640,
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            top: 20,
+            left: 20,
+            right: 20,
+          ),
+          child: Form(
+            key: formKey,
+            child: ListView(
+              // mainAxisSize: MainAxisSize.min,
+              // crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                DropdownButton<SpendCategory>(
-                    value: SpendCategory.food,
-                    items: items.map((item) {
-                      return DropdownMenuItem<SpendCategory>(
-                        value: item,
-                        child: Text(item.name),
-                      );
-                    }).toList(),
-                    onChanged: (newValue) {
-                      // setState(() {
-                      //   dropdownvalue = newValue!;
-                      // });
-                    }),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                  ),
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      // Todo: Save the data, show alert dialog
+                const SizedBox(height: 20),
+                const Text(
+                  "Title",
+                  style: TextStyle(fontSize: 15, color: Colors.blue),
+                ),
+                TextFormField(
+                  maxLength: 50,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
                     }
+                    return null;
                   },
-                  child: const Text(
-                    'Save',
-                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your Title',
+                    border: OutlineInputBorder(),
+                    counter: Offstage(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 15),
                   ),
                 ),
+                const Text(
+                  "Description",
+                  style: TextStyle(fontSize: 15, color: Colors.blue),
+                ),
+                TextFormField(
+                  maxLength: 150,
+                  minLines: 1,
+                  maxLines: 10,
+                  controller: descriptionController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your Description',
+                    border: OutlineInputBorder(),
+                    counter: Offstage(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                  ),
+                ),
+                const Text(
+                  "Amount",
+                  style: TextStyle(fontSize: 15, color: Colors.blue),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    } else if (int.parse(value) <= 0) {
+                      return 'Please enter a valid amount';
+                    } else {
+                      return null;
+                    }
+                  },
+                  maxLength: 10,
+                  controller: amountController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your amount',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.currency_rupee),
+                    counter: Offstage(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                  ),
+                ),
+                const Text(
+                  "Category",
+                  style: TextStyle(fontSize: 15, color: Colors.blue),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    DropdownButton<SpendCategory>(
+                        value: dropdownValue,
+                        items: items.map((item) {
+                          return DropdownMenuItem<SpendCategory>(
+                            value: item,
+                            child: Text(item.name),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          // Workaround for the dropdown not updating
+                          setState(() {
+                            dropdownValue = newValue!;
+                          });
+                        }),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                      ),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          // Todo: Save the data, show alert dialog
+                          final txnModel = TransactionModel(
+                            title: titleController.text,
+                            description: descriptionController.text,
+                            amount: int.parse(amountController.text),
+                            category: dropdownValue.name,
+                            createdAt: DateTime.now().toString(),
+                            id: const Uuid().v4(),
+                            spendFrom: widget.spentFrom.name,
+                          );
+                          TransactionApiService.createDoc(
+                            txnModel,
+                            FirebaseAuth.instance.currentUser!.uid,
+                          );
+                          if (widget.spentFrom == SpendFrom.stipend) {
+                            BlocProvider.of<DashboardBloc>(context)
+                                .add(DashboardIndexChangedEvent(1));
+                          } else {
+                            BlocProvider.of<DashboardBloc>(context)
+                                .add(DashboardIndexChangedEvent(2));
+                          }
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      child: const Text(
+                        'Save',
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
               ],
             ),
-            const SizedBox(height: 20),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
-    ;
   }
 }
--- lib/features/dashboard/ui/mt_dashboard.dart
+++ lib/features/dashboard/ui/mt_dashboard.dart
@@ -4,6 +4,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../core/api_service/firebase_crud_service/utils/categories.dart';
 import '../../../core/functions/general_functions.dart';
 import '../../../design/widgets/mt_bottom_navbar.dart';
 import '../../../design/widgets/mt_bottom_sheet.dart';
@@ -102,28 +103,37 @@ class _MtDashboardState extends State<MtDashboard> {
                 } else if (state is DashboardErrorState) {
                   scaffoldMessage(context, state.message);
                 } else if (state is StipendPageLoadedState) {
-                  // TODO: show modal bottom sheet
                   showModalBottomSheet(
                     isScrollControlled: true,
                     shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20))),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
                     context: context,
                     builder: (context) {
-                      return MtBottomSheet();
+                      return const MtBottomSheet(
+                        spentFrom: SpendFrom.stipend,
+                      );
                     },
                   );
-                  // TransactionApiService.createDoc(
-                  //   txnModel,
-                  //   FirebaseAuth.instance.currentUser!.uid,
-                  // );
                 } else if (state is AllowancePageLoadedState) {
-                  // TODO: show modal bottom sheet
-                  // TransactionApiService.createDoc(
-                  //   txnModel,
-                  //   FirebaseAuth.instance.currentUser!.uid,
-                  // );
+                  showModalBottomSheet(
+                    isScrollControlled: true,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    context: context,
+                    builder: (context) {
+                      return const MtBottomSheet(
+                        spentFrom: SpendFrom.allowance,
+                      );
+                    },
+                  );
                 }
               },
               tooltip: 'Add Transaction',

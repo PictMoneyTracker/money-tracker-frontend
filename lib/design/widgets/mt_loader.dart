c65bd3ae493aa49ef292f03d3f5c88278b6d82a1
Purva Mundada <84332986+mundadapurva@users.noreply.github.com>
2023-03-07 19:02:08 +0530

    Features/transaction io (#20)
    * setting page converted to form
    
    * settings page and transaction card ui implemented
    
    * styled loader and balance card

--- lib/design/widgets/mt_loader.dart
+++ lib/design/widgets/mt_loader.dart
@@ -7,6 +7,19 @@ class MtLoader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CircularProgressIndicator();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Text('Track your money, easily!',
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+        SizedBox(height: 10),
+        // CircularProgressIndicator(),
+        Image(
+          image: NetworkImage(
+              'https://media.tenor.com/yQPfHp6AmGgAAAAi/money-with-wings-joypixels.gif'),
+          height: 60,
+        ),
+      ],
+    );
   }
 }
